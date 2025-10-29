fb0e8636c52976dc782f57bb0e662fbd523bd282
Rafael Ramunieh <rafaelramunieh@hotmail.com>
2025-10-29 12:44:39 -0300

    Update Pessoa.py

--- lista16/Pessoa.py
+++ lista16/Pessoa.py
@@ -5,7 +5,12 @@ class Pessoa ():
         self.__telefone = telefone
         self.__email = email
     
+    def getNome(self):
+        return self.__nome
+
     def informaDescricao(self):
-        p = f'Pessoa {self.__nome}'
+        nome_pessoa = self.getNome()
+        nome_classe = self.__class__.__name__
+        p = f'{nome_pessoa} {nome_classe}'
         return p
         
